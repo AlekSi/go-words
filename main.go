1cbf967f6483d20c2fbd42ecdad9ca7a48e631e7
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2016-06-22 10:48:49 +0300

    Process standard library.

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"go/ast"
+	"go/build"
+	"go/parser"
+	"go/token"
+	"log"
+	"os/exec"
+	"path/filepath"
 	"regexp"
 	"sort"
+	"strings"
 )
 
 var (
@@ -33,26 +42,120 @@ var (
 	wordsRE = regexp.MustCompile(`^([[:alpha:]]+)`)
 )
 
-func addWords(words []string) {
+var DebugF = flag.Bool("debug", false, "Enable debug output")
+
+func debugf(format string, v ...interface{}) {
+	if *DebugF {
+		log.Printf(format, v...)
+	}
+}
+
+// addWords adds words to Words map, stripping non-alpha suffixes.
+// For example, "uint8" and "uint16" both result in "uint".
+func addWords(words ...string) {
 	var m []string
 	for _, w := range words {
 		m = wordsRE.FindStringSubmatch(w)
 		if len(m) > 1 {
+			debugf("adding %q", m[1])
 			Words[m[1]] = struct{}{}
 		}
 	}
 }
 
-func main() {
-	addWords(keywords)
-	addWords(builtin)
+// processIdent extracts words from ident and adds them to Words.
+func processIdent(ident *ast.Ident) {
+	if ident == nil {
+		return
+	}
 
+	if strings.Contains(ident.Name, ".") {
+		log.Fatal("unhandled ident %q", ident.Name)
+	}
+
+	if ast.IsExported(ident.Name) {
+		addWords(ident.Name)
+	}
+}
+
+// processAST extracts words from f.
+func processAST(f *ast.File) {
+	for _, decl := range f.Decls {
+		switch decl := decl.(type) {
+		case *ast.GenDecl:
+			for _, spec := range decl.Specs {
+				switch spec := spec.(type) {
+				case *ast.ImportSpec:
+					processIdent(spec.Name)
+
+				case *ast.ValueSpec:
+					for _, n := range spec.Names {
+						processIdent(n)
+					}
+
+				case *ast.TypeSpec:
+					processIdent(spec.Name)
+
+				default:
+					log.Fatalf("unhandled spec %#v", spec)
+				}
+			}
+
+		case *ast.FuncDecl:
+			processIdent(decl.Name)
+
+		default:
+			log.Fatalf("unhandled decl %#v", decl)
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	log.SetFlags(log.Lshortfile)
+
+	// add keywords and builtin
+	addWords(keywords...)
+	addWords(builtin...)
+
+	// get std packages
+	cmd := exec.Command("go", "list", "std")
+	b, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	packages := strings.Split(strings.TrimSuffix(string(b), "\n"), "\n")
+
+	// process packages
+	for _, p := range packages {
+		pack, err := build.Import(p, "", 0)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// skip internal packages like "internal/trace" or "net/http/internal"
+		if strings.Contains(pack.ImportPath, "internal") {
+			debugf("skipping internal package %q", pack.ImportPath)
+			continue
+		}
+		debugf("processing package %q", pack.ImportPath)
+
+		fset := token.NewFileSet()
+		for _, f := range pack.GoFiles {
+			ast, err := parser.ParseFile(fset, filepath.Join(pack.Dir, f), nil, 0)
+			if err != nil {
+				log.Fatal(err)
+			}
+			processAST(ast)
+		}
+	}
+
+	// sort and print result
 	res := make([]string, 0, len(Words))
 	for w := range Words {
 		res = append(res, w)
 	}
 	sort.Strings(res)
-
 	for _, w := range res {
 		fmt.Println(w)
 	}
